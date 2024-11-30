3363cbd7d12440d27fafc2787ca1697093f9fa05
Justin Schmitz <schmitzjustin2000@gmail.com>
2024-11-29 23:46:46 -0600

    Better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var MAIN_CONTEXT = MAIN_CANVAS.getContext('2d');
 var materialSelect = document.getElementById("materialSelect");
 var fastTerrain = document.getElementById("fastTerrain");
 
-var selectedMaterial = "dirt";
+var selectedMaterial = "static";
 
 materialSelect.addEventListener('change', (e) => selectedMaterial = e.target.value);
 MAIN_CANVAS.addEventListener('mousemove', handleClick, false);
@@ -11,6 +11,7 @@ MAIN_CANVAS.addEventListener('mousemove', handleClick, false);
 var mouseDown = 0;
 var lastClickEvent = null;
 var lastTick = Date.now();
+
 
 document.body.onmousedown = function () {
     mouseDown = 1;
@@ -23,8 +24,10 @@ document.body.onmouseup = function () {
 // 'little guys' may aquire multiple squares
 const BASE_SIZE = 8;
 const MILLIS_PER_TICK = 2;
-var CANVAS_SQUARES_X = 256 * 2;
-var CANVAS_SQUARES_Y = 128 * 2;
+var CANVAS_SQUARES_X = 256;
+var CANVAS_SQUARES_Y = 128;
+var ERASE_RADIUS = 2;
+var lastLastClickEvent = null;
 
 MAIN_CANVAS.width = CANVAS_SQUARES_X * BASE_SIZE;
 MAIN_CANVAS.height = CANVAS_SQUARES_Y * BASE_SIZE;
@@ -32,6 +35,35 @@ MAIN_CANVAS.height = CANVAS_SQUARES_Y * BASE_SIZE;
 var ALL_SQUARES = {}
 var DIRTY_SQUARES = [];
 var NEXT_DIRTY_SQUARES = [];
+
+var rightMouseClicked = false;
+
+function handleMouseDown(e) {
+  //e.button describes the mouse button that was clicked
+  // 0 is left, 1 is middle, 2 is right
+  if (e.button === 2) {
+    rightMouseClicked = true;
+  } else if (e.button === 0) {  
+    //Do something if left button was clicked and right button is still pressed
+    if (rightMouseClicked) {
+      console.log('hello');
+      //code
+    }
+  }
+}
+
+function handleMouseUp(e) {
+  if (e.button === 2) {
+    rightMouseClicked = false;
+  }
+}
+
+document.addEventListener('mousedown', handleMouseDown);
+document.addEventListener('mouseup', handleMouseUp);
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
 
 class BaseSquare {
     constructor(posX, posY) {
@@ -44,9 +76,9 @@ class BaseSquare {
         this.blockHealth = 1; // when reaches zero, delete
         // water flow parameters
         this.waterContainment = 0;
-        this.waterContainmentMax = 0.5;
-        this.waterContainmentFillRate = 0.25;
-        this.waterContainmentTransferRate = 0.0005; // what fraction of ticks does it trigger percolate on
+        this.waterContainmentMax = 0.75;
+        this.waterContainmentFillRate = 0.05;
+        this.waterContainmentTransferRate = 0.8; // what fraction of ticks does it trigger percolate on
         this.waterContainmentEvaporationRate = 0.0005; // what fraction of contained water will get reduced per tick
         this.evaporationRate = 0;
         this.falling = false;
@@ -241,6 +273,17 @@ class StaticSquare extends BaseSquare {
     }
 }
 
+class DrainSquare extends BaseSquare {
+    constructor(posX, posY) {
+        super(posX, posY);
+        this.colorBase = "#000500";
+        this.physicsEnabled = false;
+        this.waterContainmentMax = 100;
+        this.waterContainmentFillRate = 1;
+        this.waterContainmentTransferRate = 1;
+    }
+}
+
 class RainSquare extends StaticSquare {
     constructor(posX, posY) {
         super(posX, posY);
@@ -412,6 +455,14 @@ function getSquare(posX, posY) {
     return ALL_SQUARES[posX][posY];
 }
 
+function removeSquarePos(x, y) {
+    x = Math.floor(x);
+    y = Math.floor(y);
+    if (getSquare(x, y) != null) {
+        removeSquare(getSquare(x, y));
+
+    }
+}
 function removeSquare(square) {
     ALL_SQUARES[square.posX][square.posY] = null;
 }
@@ -482,7 +533,7 @@ function main() {
 }
 
 for (let i = 0; i < CANVAS_SQUARES_X; i++) {
-    addSquare(new StaticSquare(i, CANVAS_SQUARES_Y - 1));
+    addSquare(new DrainSquare(i, CANVAS_SQUARES_Y - 1));
 }
 
 // for (let i = 0; i < 5000; i++) {
@@ -497,6 +548,9 @@ function randNumber(min, max) {
 
 function handleClick(event) {
     lastClickEvent = event;
+    if (!rightMouseClicked && mouseDown <= 0) {
+        lastLastClickEvent = event;
+    }
 }
 
 function doClickAdd() {
@@ -506,31 +560,74 @@ function doClickAdd() {
     if (mouseDown > 0) {
         var offsetX = lastClickEvent.offsetX / BASE_SIZE;
         var offsetY = lastClickEvent.offsetY / BASE_SIZE;
-        for (let i = 0; i < (CANVAS_SQUARES_Y - offsetY); i++) {
-            var curY = offsetY + i;
-            switch (selectedMaterial) {
-                case "static":
-                    addSquare(new StaticSquare(offsetX, curY));
+        var prevOffsetX = (lastLastClickEvent == null ? lastClickEvent : lastLastClickEvent).offsetX / BASE_SIZE;
+        var prevOffsetY = (lastLastClickEvent == null ? lastClickEvent : lastLastClickEvent).offsetY / BASE_SIZE;
+        var dirX = (offsetX - prevOffsetX > 0 ? 1 : -1);
+        var dirY = (offsetY - prevOffsetY > 0 ? 1 : -1);
+
+        // point slope motherfuckers 
+
+        var x1 = prevOffsetX;
+        var x2 = offsetX;
+        var y1 = prevOffsetY; 
+        var y2 = offsetY;
+
+        var dx = x2 - x1; 
+        var dy = y2 - y1;
+        var dz = Math.pow(dx ** 2 + dy ** 2, 0.5);
+    
+        var totalCount = Math.round(dz);
+        var ddx = dx / totalCount;
+        var ddy = dy / totalCount; 
+        for (let i = 0; i < totalCount; i++) {
+            var px = x1 + ddx * i;
+            var py = y1 + ddy * i;
+            for (let i = 0; i < (CANVAS_SQUARES_Y - offsetY); i++) {
+                var curY = py + i;
+                if (rightMouseClicked) {
+                    doErase(px, curY);
                     break;
-                case "dirt":
-                    addSquare(new DirtSquare(offsetX, curY));
+                } else {
+                    switch (selectedMaterial) {
+                        case "static":
+                            addSquare(new StaticSquare(px, curY));
+                            break;
+                        case "dirt":
+                            addSquare(new DirtSquare(px, curY));
+                            break;
+                        case "water":
+                            addSquare(new WaterSquare(px, curY));
+                            break;
+                        case "rain":
+                            addSquare(new RainSquare(px, curY));
+                            break;
+                        case "heavy rain":
+                            addSquare(new HeavyRainSquare(px, curY));
+                            break;
+                        case "drain":
+                            addSquare(new DrainSquare(px, curY));
+                            break;
+                    }
+                }
+                if (!fastTerrain.checked) {
                     break;
-                case "water":
-                    addSquare(new WaterSquare(offsetX, curY));
-                    break;
-                case "rain":
-                    addSquare(new RainSquare(offsetX, curY));
-                    break;
-                case "heavy rain":
-                    addSquare(new HeavyRainSquare(offsetX, curY));
-                    break;
-            }
-            if (!fastTerrain.checked) {
-                break;
+                }
             }
         }
+        lastLastClickEvent = lastClickEvent;
     }
+}
 
+function doErase(x, y) {
+    var workingEraseRadius = ERASE_RADIUS * 2 + 1;
+    // it has to be an odd number
+    // we make a cross like thing
+    var start = (workingEraseRadius + 1) / 2;
+    for (var i = -start; i < start; i++) {
+        for (var j = -start; j < start; j++) {
+            removeSquarePos(x + i, y + j);
+        }
+    }
 }
 
 
@@ -554,3 +651,9 @@ function rgbToHex(r, g, b) {
 setInterval(main, 1);
 
 // setTimeout(() => window.location.reload(), 3000);
+window.oncontextmenu = function () {
+    return false;     // cancel default menu
+  }
+
+
+  
