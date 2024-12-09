9a73c8ef59f641d1bb81c74614d6a4577e16a8d8
Justin Schmitz <schmitzjustin2000@gmail.com>
2024-12-08 20:38:43 -0600

    CONFIGURATION!!!!

--- index.js
+++ index.js
@@ -48,6 +48,145 @@ var WATERFLOW_CANDIDATE_SQUARES = new Set();
 
 var rightMouseClicked = false;
 
+setInterval(displayConfigs, 1);
+
+var all_configs = [];
+
+var displayConfigDirty = false;
+var displayConfigText = "";
+
+function addConfig(config) {
+    all_configs.push(config);
+    var newSlider = document.createElement("input");
+    newSlider.type = "range";
+    newSlider.class = "slider";
+    newSlider.value = config.value;
+    newSlider.min = config.value / 2;
+    newSlider.max = config.value * 2;
+    newSlider.step = config.value / 1000;
+    newSlider.id = "slider_" + config.name;
+    configSliders.appendChild(newSlider);
+    var label = document.createElement("label");
+    label.innerText = "slider_" + config.name + "\tvalue: " + config.value;
+    label.htmlFor = "slider_" + config.name;
+    configSliders.appendChild(label);
+    configSliders.append(document.createElement("br"));
+    newSlider.onchange = (e) => {
+        config.value = e.target.value;
+        displayConfigDirty = true;
+        label.innerText = "slider_" + config.name + "\tvalue: " + e.target.value;
+    };
+}
+
+
+var b_sq_waterContainmentMax = {
+    name: "b_sq_waterContainmentMax",
+    value: 0.2
+};
+var b_sq_waterContainmentTransferRate = {
+    name: "b_sq_waterContainmentTransferRate",
+    value: 0.3
+};
+var b_sq_waterContainmentEvaporationRate = {
+    name: "b_sq_waterContainmentEvaporationRate",
+    value: 0.0005
+};
+var b_sq_darkeningStrength = {
+    name: "b_sq_darkeningStrength",
+    value: 0.3
+};
+var d_sq_nutrientValue = {
+    name: "d_sq_nutrientValue",
+    value: 0.05
+};
+var rain_dropChance = {
+    name: "rain_dropChance",
+    value: 0.001
+};
+var heavyrain_dropChance = {
+    name: "heavyrain_dropChance",
+    value: 0.02
+};
+var water_evaporationRate ={
+    name: "water_evaporationRate",
+    value:  0
+};
+var water_viscocity = {
+    name: "water_viscocity",
+    value: 0.1
+};
+var water_darkeningStrength = {
+    name: "water_darkeningStrength",
+    value: 0.3
+};
+var po_airSuckFrac = {
+    name: "po_airSuckFrac",
+    value: 0.2
+};
+var po_waterSuckFrac = {
+    name: "po_waterSuckFrac",
+    value: 0.2
+};
+var po_rootSuckFrac = {
+    name: "po_rootSuckFrac",
+    value: 0.2
+};
+var po_perFrameCostFracPerSquare = {
+    name: "po_perFrameCostFracPerSquare",
+    value: .0002
+};
+var po_greenSquareSizeExponentCost = {
+    name: "po_greenSquareSizeExponentCost",
+    value: 1.5
+};
+var po_rootSquareSizeExponentCost = {
+    name: "po_rootSquareSizeExponentCost",
+    value: 1.5
+};
+var p_ls_airNutrientsPerExposedNeighborTick = {
+    name: "p_ls_airNutrientsPerExposedNeighborTick",
+    value: 0.0025
+};
+var p_seed_ls_sproutGrowthRate = {
+    name: "p_seed_ls_sproutGrowthRate",
+    value: 0.01
+};
+var p_seed_ls_neighborWaterContainmentRequiredToGrow = {
+    name: "p_seed_ls_neighborWaterContainmentRequiredToGrow",
+    value: 0.8
+};
+var p_seed_ls_neighborWaterContainmentRequiredToDecay = {
+    name: "p_seed_ls_neighborWaterContainmentRequiredToDecay",
+    value: 0.1
+};
+var p_seed_ls_darkeningStrength = {
+    name: "p_seed_ls_darkeningStrength",
+    value: 0.3
+};
+
+addConfig(b_sq_waterContainmentMax);
+addConfig(b_sq_waterContainmentTransferRate);
+addConfig(b_sq_waterContainmentEvaporationRate);
+addConfig(b_sq_darkeningStrength);
+addConfig(d_sq_nutrientValue);
+addConfig(rain_dropChance);
+addConfig(heavyrain_dropChance);
+addConfig(water_evaporationRate);
+addConfig(water_viscocity);
+addConfig(water_darkeningStrength);
+addConfig(po_airSuckFrac);
+addConfig(po_waterSuckFrac);
+addConfig(po_rootSuckFrac);
+addConfig(po_perFrameCostFracPerSquare);
+addConfig(po_greenSquareSizeExponentCost);
+addConfig(po_rootSquareSizeExponentCost);
+addConfig(p_ls_airNutrientsPerExposedNeighborTick);
+addConfig(p_seed_ls_sproutGrowthRate);
+addConfig(p_seed_ls_neighborWaterContainmentRequiredToGrow);
+addConfig(p_seed_ls_neighborWaterContainmentRequiredToDecay);
+addConfig(p_seed_ls_darkeningStrength);
+
+
 loadSlotA.onclick = (e) => loadSlot("A");
 saveSlotA.onclick = (e) => saveSlot("A");
 
@@ -1610,35 +1749,6 @@ var ProtoMap = {
 }
 
 
-var all_configs = [];
-
-var displayConfigDirty = false;
-var displayConfigText = "";
-
-
-function addConfig(config) {
-    all_configs.push(config);
-    var newSlider = document.createElement("input");
-    newSlider.type = "range";
-    newSlider.class = "slider";
-    newSlider.value = config.value;
-    newSlider.min = config.value / 10;
-    newSlider.max = config.value * 10;
-    newSlider.step = config.value / 1000;
-    newSlider.id = "slider_" + config.name;
-    newSlider.onchange = (e) => {
-        config.value = e.target.value;
-        displayConfigDirty = true;
-    };
-
-    configSliders.appendChild(newSlider);
-    var label = document.createElement("label");
-    label.innerText = "slider_" + config.name;
-    label.htmlFor = "slider_" + config.name;
-    configSliders.appendChild(label);
-    configSliders.append(document.createElement("br"));
-}
-
 function displayConfigs() {
     if (!displayConfigDirty) {
         return;
@@ -1659,38 +1769,4 @@ var bar = {
     name: "bar",
     value: 88.10352
 };
-addConfig(foo);
-addConfig(bar);
 
-setInterval(displayConfigs, 1);
-
-
-var b_sq_waterContainmentMax = 0.2;
-var b_sq_waterContainmentTransferRate = 0.3;
-var b_sq_waterContainmentEvaporationRate = 0.0005;
-var b_sq_darkeningStrength = 0.3;
-
-var d_sq_nutrientValue = 0.05;
-
-var rain_dropChance = 0.001;
-var heavyrain_dropChance = 0.02;
-
-var water_evaporationRate = 0; 
-var water_viscocity = 0.1;
-var water_darkeningStrength = 0.3;
-
-var po_airSuckFrac = 0.2;
-var po_waterSuckFrac = 0.2;
-var po_rootSuckFrac = 0.2;
-
-var po_perFrameCostFracPerSquare = (1 / 500);
-
-var po_greenSquareSizeExponentCost = 1.5;
-var po_rootSquareSizeExponentCost = 1.5;
-
-var p_ls_airNutrientsPerExposedNeighborTick = 0.0025;
-
-var p_seed_ls_sproutGrowthRate = 0.01;
-var p_seed_ls_neighborWaterContainmentRequiredToGrow = 0.8;
-var p_seed_ls_neighborWaterContainmentRequiredToDecay = 0.1;
-var p_seed_ls_darkeningStrength = 0.3;
