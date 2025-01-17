ca165179e5ce2a26cc7bf544a18da6a9a59bbc84
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-01-17 16:48:40 -0600

    checkpoint

--- static/lifeSquares/parameterized/woodland/PalmTreeGreenSquare.js
+++ static/lifeSquares/parameterized/woodland/PalmTreeGreenSquare.js
@@ -1,13 +1,53 @@
 import { getNeighbors } from "../../../squares/_sqOperations.js";
 import { BaseLifeSquare } from "../../BaseLifeSquare.js";
 import { airNutrientsPerEmptyNeighbor } from "../../../config/config.js";
+import { SUBTYPE_LEAF, SUBTYPE_LEAFSTEM, SUBTYPE_NODE, SUBTYPE_SHOOT, SUBTYPE_SPROUT, SUBTYPE_TRUNK, SUBTYPE_TRUNK_CORE } from "../../../organisms/parameterized/Stages.js";
+import { hexToRgb } from "../../../common.js";
 
 export class PalmTreeGreenSquare extends BaseLifeSquare {
     constructor(square, organism) {
         super(square, organism);
         this.proto = "PalmTreeGreenSquare";
         this.type = "green";
+
+        this.activeRenderSubtype = null;
     }
+
+    
+    subtypeColorUpdate() {
+        switch (this.subtype) {
+            case SUBTYPE_TRUNK:
+            case SUBTYPE_TRUNK_CORE:
+            case SUBTYPE_SHOOT:
+            case SUBTYPE_SPROUT:
+                this.baseColor = "#615041";
+                this.darkColor = "#8e7b6a";
+                this.accentColor = "#351e0f";
+                break;
+            case SUBTYPE_LEAF:
+            case SUBTYPE_LEAFSTEM:
+            case SUBTYPE_NODE:
+                this.baseColor = "#6c7d45";
+                this.darkColor = "#7a7106";
+                this.accentColor = "#d0ce24";
+                break;
+            default:
+                console.warn("BIPPITY BOPPITY")
+        }
+
+        this.activeRenderSubtype = this.subtype;
+        this.baseColor_rgb = hexToRgb(this.baseColor);
+        this.darkColor_rgb = hexToRgb(this.darkColor);
+        this.accentColor_rgb = hexToRgb(this.accentColor);
+
+    }
+
+    preTick() {
+        if (this.activeRenderSubtype != this.subtype) {
+            this.subtypeColorUpdate();
+        }
+    }
+
 
     tick() {
         this.addAirNutrient(
