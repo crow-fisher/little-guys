75ee4ddf7045ceeb75ce9eda887c765ef2b7f83c
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-02-01 18:31:51 -0600

    checkpoint

--- static/index.js
+++ static/index.js
@@ -215,8 +215,8 @@ var lastTick = Date.now();
 
 // wind is tiled x4
 
-var CANVAS_SQUARES_X = 20 * 4; // * 8; //6;
-var CANVAS_SQUARES_Y = 20 * 4; // * 8; // 8;
+var CANVAS_SQUARES_X = (192)/8 * 4; // * 8; //6;
+var CANVAS_SQUARES_Y =  (108)/8 * 4; // * 8; // 8;
 
 
 function setCanvasSquaresX(val) {
