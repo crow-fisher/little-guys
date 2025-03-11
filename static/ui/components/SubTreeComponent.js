09c16095feac7c80f9e1e69b534a5e6892d0a97b
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-03-11 17:25:57 -0500

    add diagonals doofus

--- static/ui/components/SubTreeComponent.js
+++ static/ui/components/SubTreeComponent.js
@@ -1,4 +1,4 @@
-import { getBaseUISize } from "../../canvas.js";
+import { getBaseUISize, getCanvasWidth } from "../../canvas.js";
 import { getActiveClimate } from "../../climate/climateManager.js";
 import { MAIN_CONTEXT } from "../../index.js";
 import { loadEmptyScene, loadSlot, saveSlot } from "../../saveAndLoad.js";
@@ -23,20 +23,37 @@ export class SubTreeComponent {
     render() {
         if (loadUI(this.key)) {
             this.window.render();
-            let size = getBaseUISize() * 0.4;
+            let size = getBaseUISize() * 1.3;
+
+            let py = this.window.posY + this.window.sizeY;
+            let my = getCanvasWidth() / 2;
+            if (py > my) {
+                return;
+            }
+            let sizeYProcessed = size * ((my - py) / my);
+
+            let px = this.window.posX + this.window.sizeX;
+            let mx = getCanvasWidth() * 0.66; 
+            if (px > mx) {
+                return;
+            }
+            let sizeXProcessed = size * ((mx - px) / mx)
+
             MAIN_CONTEXT.fillStyle = getActiveClimate().getUIColorInactiveCustom(0.99);
             MAIN_CONTEXT.fillRect(
                 this.window.posX,
                 this.window.posY + this.window.sizeY,
-                this.window.sizeX + size,
-                (size * 0.7)
+                this.window.sizeX + sizeXProcessed,
+                sizeYProcessed
             );
+
+            // right side
 
             MAIN_CONTEXT.fillStyle = getActiveClimate().getUIColorInactiveCustom(0.8);
             MAIN_CONTEXT.fillRect(
                 this.window.posX + this.window.sizeX,
                 this.window.posY,
-                size,
+                sizeXProcessed,
                 this.window.sizeY
             );
         }
