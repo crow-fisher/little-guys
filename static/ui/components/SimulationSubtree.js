fb8fb7864aa5e773ad92ef81824e9b2d308f704a
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-03-29 12:00:22 -0500

    checkpoint

--- static/ui/components/SimulationSubtree.js
+++ static/ui/components/SimulationSubtree.js
@@ -21,10 +21,12 @@ export class SimulationSubtree extends SubTreeComponent {
         subMenuContainer.addElement(new Toggle(this.window, sizeX + textAlignOffsetX, getBaseUISize() * 3, textAlignOffsetX, UI_SIMULATION_SIMPLESQUARE, "simple physics",() => getActiveClimate().getUIColorInactiveCustom(0.61), () => getActiveClimate().getUIColorActive()));
         subMenuContainer.addElement(new Text(this.window, sizeX + textAlignOffsetX, getBaseUISize() * 3, UI_CENTER, "size"))
 
+    
         let row1 =  new Container(this.window, 0, 0);
         let row2 =  new Container(this.window, 0, 0);
         let row3 =  new Container(this.window, 0, 0);
         let row4 =  new Container(this.window, 0, 0);
+        let row5 =  new Container(this.window, 0, 0);
 
         row1.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 75,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
         row1.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 100,() => getActiveClimate().getUIColorInactiveCustom(0.55), () => getActiveClimate().getUIColorActive()));
@@ -35,25 +37,30 @@ export class SimulationSubtree extends SubTreeComponent {
         row3.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 225,() => getActiveClimate().getUIColorInactiveCustom(0.63), () => getActiveClimate().getUIColorActive()));
         row3.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 250,() => getActiveClimate().getUIColorInactiveCustom(0.59), () => getActiveClimate().getUIColorActive()));
         row3.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 275,() => getActiveClimate().getUIColorInactiveCustom(0.64), () => getActiveClimate().getUIColorActive()));
-        row4.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 400,() => getActiveClimate().getUIColorInactiveCustom(0.60), () => getActiveClimate().getUIColorActive()));
-        row4.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 450,() => getActiveClimate().getUIColorInactiveCustom(0.56), () => getActiveClimate().getUIColorActive()));
-        row4.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 500,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
+        row4.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 300,() => getActiveClimate().getUIColorInactiveCustom(0.60), () => getActiveClimate().getUIColorActive()));
+        row4.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 350,() => getActiveClimate().getUIColorInactiveCustom(0.56), () => getActiveClimate().getUIColorActive()));
+        row4.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 400,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
+        row5.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 450,() => getActiveClimate().getUIColorInactiveCustom(0.57), () => getActiveClimate().getUIColorActive()));
+        row5.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 500,() => getActiveClimate().getUIColorInactiveCustom(0.61), () => getActiveClimate().getUIColorActive()));
+        row5.addElement(new RadioToggle(this.window, radioSizeX + (textAlignOffsetX / 3), getBaseUISize() * 3, UI_CENTER, UI_SIMULATION_HEIGHT, 550,() => getActiveClimate().getUIColorInactiveCustom(0.58), () => getActiveClimate().getUIColorActive()));
         
+
         subMenuContainer.addElement(row1);
         subMenuContainer.addElement(row2);
         subMenuContainer.addElement(row3);
         subMenuContainer.addElement(row4);
-
-        let row5 =  new Container(this.window, 0, 0);
-        let row6 =  new Container(this.window, 0, 0);
-        subMenuContainer.addElement(new Text(this.window, sizeX + textAlignOffsetX, getBaseUISize() * 2, UI_CENTER, "generation cycle length"));
         subMenuContainer.addElement(row5);
-        subMenuContainer.addElement(row6);
 
-        row5.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "days", UI_SIMULATION_GENS_PER_DAY, 1 / 2,() => getActiveClimate().getUIColorInactiveCustom(0.56), () => getActiveClimate().getUIColorActive()));
-        row5.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "hours", UI_SIMULATION_GENS_PER_DAY, 10,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
-        row6.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "minutes", UI_SIMULATION_GENS_PER_DAY, 250,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
-        row6.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "seconds", UI_SIMULATION_GENS_PER_DAY, 5000,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
+        let row6 =  new Container(this.window, 0, 0);
+        let row7 =  new Container(this.window, 0, 0);
+        subMenuContainer.addElement(new Text(this.window, sizeX + textAlignOffsetX, getBaseUISize() * 2, UI_CENTER, "generation cycle length"));
+        subMenuContainer.addElement(row6);
+        subMenuContainer.addElement(row7);
+
+        row6.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "days", UI_SIMULATION_GENS_PER_DAY, 1 / 2,() => getActiveClimate().getUIColorInactiveCustom(0.56), () => getActiveClimate().getUIColorActive()));
+        row6.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "hours", UI_SIMULATION_GENS_PER_DAY, 10,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
+        row7.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "minutes", UI_SIMULATION_GENS_PER_DAY, 250,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
+        row7.addElement(new RadioToggleLabel(this.window, sizeX / 2 + (textAlignOffsetX / 2), getBaseUISize() * 3, UI_CENTER, "seconds", UI_SIMULATION_GENS_PER_DAY, 5000,() => getActiveClimate().getUIColorInactiveCustom(0.62), () => getActiveClimate().getUIColorActive()));
     }
 
 
