5e76b7ed7ab5dbb76fd03536cde9a47bed69a4bd
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-04-20 11:03:43 -0500

    checkpoint

---
+++ static/ui/elements/SliderGradientBackground.js
@@ -0,0 +1,61 @@
+import { getBaseUISize } from "../../canvas.js";
+import { calculateColor } from "../../climate/temperatureHumidity.js";
+import { COLOR_BLACK, COLOR_BLUE, COLOR_OTHER_BLUE, COLOR_VERY_FUCKING_RED } from "../../colors.js";
+import { MAIN_CONTEXT } from "../../index.js";
+import { isLeftMouseClicked } from "../../mouse.js";
+import { loadGD, saveGD } from "../UIData.js";
+import { WindowElement } from "../Window.js";
+
+export class SliderGradientBackground extends WindowElement {
+    constructor(window, key, sizeX, sizeY, min, max, minColorFunc, maxColorFunc) {
+        super(window, sizeX, sizeY);
+        this.key = key;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.min = min;
+        this.max = max;
+        this.minColorFunc = minColorFunc;
+        this.maxColorFunc = maxColorFunc;
+    }
+
+    render(startX, startY) {
+        let gradient = MAIN_CONTEXT.createLinearGradient(startX, startY, this.sizeX + startX, startY);
+        gradient.addColorStop(0, this.minColorFunc());
+        gradient.addColorStop(1, this.maxColorFunc());
+        MAIN_CONTEXT.fillStyle = gradient;
+        MAIN_CONTEXT.fillRect(startX, startY, this.sizeX, this.sizeY);
+
+        let blockSize = this.sizeY;
+        let invlerp = (loadGD(this.key) - this.min) / (this.max - this.min);
+        let lerp = invlerp * this.sizeX;
+
+        MAIN_CONTEXT.fillStyle = calculateColor(invlerp, 0, 1, this.minColorFunc, this.maxColorFunc);
+        MAIN_CONTEXT.fillRect(startX + lerp - (blockSize / 2), startY, blockSize, this.sizeY);
+        MAIN_CONTEXT.fill();
+
+        let lineWidth = getBaseUISize() * 0.1;
+        MAIN_CONTEXT.strokeStyle = COLOR_BLACK;        // set the color for the circle to 'green'
+        MAIN_CONTEXT.lineWidth = lineWidth;
+
+
+        MAIN_CONTEXT.strokeRect(startX + lerp - (blockSize / 2), startY + (lineWidth / 2), blockSize, this.sizeY - (lineWidth));
+
+        return [this.sizeX, this.sizeY]
+    }
+
+    hover(posX, posY) {
+        super.hover(posX, posY);
+        if (!isLeftMouseClicked()) {
+            return;
+        }
+        this.window.locked = true;
+        let min = 0;
+        let max = this.sizeX;
+        posX = Math.max((this.sizeY / 2), posX);
+        posX = Math.min(this.sizeX - (this.sizeY / 2), posX);
+        let p = (posX - min) / (max - min);
+        p = Math.min(Math.max(0, p), 1)
+        saveGD(this.key, this.min + (p * (this.max - this.min)));
+    }
+
+}
