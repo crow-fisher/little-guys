3242b6067c6b2348a2a8506d5b262ed7c46e54a6
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-05-10 10:29:15 -0500

    checkpoint

--- static/ui/components/OrganismComponent.js
+++ static/ui/components/OrganismComponent.js
@@ -26,7 +26,9 @@ export class OrganismComponent extends Component {
 
           let h1 = getBaseUISize() * 3;
           let h2 = getBaseUISize() * 2.5;
-          let br = getBaseUISize() * .5;
+          let br1 =  getBaseUISize() * .75;
+          let br2 = getBaseUISize() * .5;
+          let br3 = Math.round(getBaseUISize() * .25);
 
           container.addElement(new TextBackground(this.window, sizeX, getBaseUISize() * 0.35, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.75), 0.75, " "))
           container.addElement(new TextBackground(this.window, sizeX, getBaseUISize() * 3.8, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.66315, "plant editor"))
@@ -37,7 +39,7 @@ export class OrganismComponent extends Component {
 
           container.addElement(modeSelectRow1);
           container.addElement(modeSelectRow2);
-          container.addElement(new TextBackground(this.window, sizeX, br, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""));
+          container.addElement(new TextBackground(this.window, sizeX, br2, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""));
 
           modeSelectRow1.addElement(new RadioToggleLabel(this.window, half, h1, offsetX, "mosses", UI_ORGANISM_TYPE_SELECT, UI_ORGANISM_TYPE_MOSS,
                () => getActiveClimate().getUIColorInactiveCustom(0.57), () => getActiveClimate().getUIColorInactiveCustom(0.50)));
@@ -65,8 +67,7 @@ export class OrganismComponent extends Component {
                () => getActiveClimate().getUIColorInactiveCustom(0.63), () => getActiveClimate().getUIColorInactiveCustom(0.53)));
           grassConditionalContainer.addElement(new RadioToggleLabel(this.window, sizeX, h1, offsetX, "cattail", UI_ORGANISM_SELECT, UI_ORGANISM_GRASS_CATTAIL,
                () => getActiveClimate().getUIColorInactiveCustom(0.68), () => getActiveClimate().getUIColorInactiveCustom(0.52)));
-          grassConditionalContainer.addElement(new TextBackground(this.window, sizeX, br, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
-
+          grassConditionalContainer.addElement(new TextBackground(this.window, sizeX, br2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
 
           let wheatConditionalContainer = new ConditionalContainer(this.window, 0, 1, () => loadGD(UI_ORGANISM_SELECT) == UI_ORGANISM_GRASS_WHEAT);
           let kblueConditionalContainer = new ConditionalContainer(this.window, 0, 1, () => loadGD(UI_ORGANISM_SELECT) == UI_ORGANISM_GRASS_KBLUE);
@@ -77,19 +78,19 @@ export class OrganismComponent extends Component {
           grassConditionalContainer.addElement(cattailConditionalContainer);
 
           wheatConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.75, "wheat: the cereal central to life", "italic"))
-          wheatConditionalContainer.addElement(new TextBackground(this.window, sizeX, br, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
+          wheatConditionalContainer.addElement(new TextBackground(this.window, sizeX, br3, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
           wheatConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.50), 0.75, "drained soils, full sun"))
           wheatConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.75, "growing time: 2 cycles"))
           wheatConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.51), 0.75, "value: 10 coins"))
 
           kblueConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.75, "kentucky blue: a boring grass", "italic"))
-          kblueConditionalContainer.addElement(new TextBackground(this.window, sizeX, br, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
+          kblueConditionalContainer.addElement(new TextBackground(this.window, sizeX, br3, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
           kblueConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.50), 0.75, "drained soils, partial sun"))
           kblueConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.75, "growing time: 1 cycle"))
           kblueConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.51), 0.75, "value: 1 coin"))
 
           cattailConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.75, "cattails: the forbidden corndogs", "italic"))
-          cattailConditionalContainer.addElement(new TextBackground(this.window, sizeX, br, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
+          cattailConditionalContainer.addElement(new TextBackground(this.window, sizeX, br3, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
           cattailConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.50), 0.75, "wet soils, partial sun"))
           cattailConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.55), 0.75, "growing time: 4 cycles"))
           cattailConditionalContainer.addElement(new TextBackground(this.window, sizeX, h2, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.51), 0.75, "value: 15 coins"))
@@ -97,9 +98,9 @@ export class OrganismComponent extends Component {
           // tree
           // end
           
-          container.addElement(new TextBackground(this.window, sizeX, br, offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
+          container.addElement(new TextBackground(this.window, sizeX, br2, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
           container.addElement(new TextBackground(this.window, sizeX, h1, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.51), 0.75, "evolution parameters"))
-          container.addElement(new TextBackground(this.window, sizeX, Math.floor(br/2) , offsetX, () => getActiveClimate().getUIColorInactiveCustom(0.58), 0.75, ""))
+          container.addElement(new TextBackground(this.window, sizeX, br3, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.85), 0.75, ""))
           container.addElement(new TextBackground(this.window, sizeX, h2, UI_CENTER, () => getActiveClimate().getUIColorInactiveCustom(0.58), 0.75, "target light level"))
 
           container.addElement(new SliderGradientBackgroundPlantConfigurator(this.window, sizeX, h1));
