2c0bf01fb68b940069ea0dc323987b1a84539fc3
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-05-21 09:32:49 -0500

    checkpoint

---
+++ push.bat
@@ -0,0 +1,1 @@
+butler push . sergeantbigbird/little-guys:web
--- static/organisms/midwest/CattailOrganism.js
+++ static/organisms/midwest/CattailOrganism.js
@@ -109,7 +109,7 @@ export class CattailOrganism extends BaseOrganism {
             startRootNode.posX, startRootNode.posY, 
             false, STAGE_ADULT, randRange(-Math.PI, Math.PI), baseDeflection, 0, 
             baseDeflection, 
-            randRange(0, 0.15), TYPE_TRUNK, 5.4);
+            randRange(0, 0.15), TYPE_TRUNK, 1.4);
         growthPlan.postConstruct = () => {
             this.originGrowth.addChild(growthPlan.component);
             this.grasses.push(this.originGrowth.getChildPath(growthPlan.component))
