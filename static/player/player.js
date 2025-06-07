182e4681e3c1ea094063b1668219dc58394c9011
Justin Schmitz <schmitzjustin2000@gmail.com>
2025-06-06 19:15:56 -0500

    checkpoint

--- static/player/player.js
+++ static/player/player.js
@@ -4,10 +4,10 @@ import { MAIN_CONTEXT } from "../index.js";
 import { getSquares } from "../squares/_sqOperations.js";
 
 
-const ET = 0b1000;
-const EB = 0b0100;
-const EL = 0b0010;
-const ER = 0b0001;
+const EN = 0b1000;
+const ES = 0b0100;
+const EW = 0b0010;
+const EE = 0b0001;
 
 export class Player {
     constructor() {
@@ -22,9 +22,6 @@ export class Player {
         this.kpyu = false;
         this.kpyd = false;
 
-        this.protos = ["RockSquare", "SoilSquare"];
-        this.bottomColMap = new Map();
-
         // configured members 
 
         this.sizeX = 2;
@@ -36,64 +33,59 @@ export class Player {
         this.kmyu = 'w';
         this.kmyd = 's';
 
+        this.kmJump = ' ';
+
         this.gravity = .1;
 
-        this.dirtAcc = 0.25;
-        this.rockAcc = 0.01;
-        this.airAcc = 0.05;
-
-        this.dirtMax = 1;
-        this.rockMax = 0.05;
-        this.airMax = this.dirtMax;
-
-        this.acc = .25;
+        this.walkAcc = 0.25;
+        this.walkMax = 1;
         this.jumpSpeed = 1;
+        this.jumpTicksLeft = 0;
 
         this.prevTickTime = Date.now();
 
         this.collisionCacheMap = {
-            ET: 0,
-            EB: 0,
-            EL: 0,
-            ER: 0
+            EN: 0,
+            ES: 0,
+            EW: 0,
+            EE: 0
         };
 
         this.initFrameCollisionMap();
     }
 
     initFrameCollisionMap() {
-        for (let proto of this.protos) {
-            this.bottomColMap.set(proto, false);
-            this.frameCollisionMap.set(proto, new Map());
+        for (let surfaceType of [true, false]) {
+            this.frameCollisionMap.set(surfaceType, new Map());
             for (let i = 0; i < this.sizeX; i++) {
-                this.frameCollisionMap.get(proto).set(i, new Map());
+                this.frameCollisionMap.get(surfaceType).set(i, new Map());
                 for (let j = 0; j < this.sizeY; j++) {
-                    this.frameCollisionMap.get(proto).get(i).set(j, false);
+                    this.frameCollisionMap.get(surfaceType).get(i).set(j, 0);
                 }
             }
         }
     }
 
     render() {
-        for (let proto of this.protos) {
+        for (let proto of [true, false]) {
             for (let i = 0; i < this.sizeX; i++) {
                 for (let j = 0; j < this.sizeY; j++) {
                     MAIN_CONTEXT.fillStyle = COLOR_BLACK;
                     zoomCanvasFillRect((this.posX + i) * getBaseSize(), (this.posY + j) * getBaseSize(), getBaseSize(), getBaseSize());
                     let colMask = this.frameCollisionMap.get(proto).get(i).get(j);
 
-                    if (proto == "RockSquare")
+                    if (proto)
                         MAIN_CONTEXT.fillStyle = "rgba(255, 0, 0, 0.5);"
-                    if (proto == "SoilSquare")
+                    else
                         MAIN_CONTEXT.fillStyle = "rgba(0, 255, 0, 0.5)";
 
-                    if ((ET & colMask) == ET)
+                    if ((EN & colMask) == EN)
                         zoomCanvasFillRect((this.posX + i) * getBaseSize(), (this.posY + j) * getBaseSize(), getBaseSize(), getBaseSize() * 0.125);
-                    if ((EB & colMask) == EB)
+                    if ((ES & colMask) == ES)
                         zoomCanvasFillRect((this.posX + i) * getBaseSize(), (this.posY + j + 1) * getBaseSize(), getBaseSize(), getBaseSize() * -0.125);
-                    if ((EL & colMask) == EL)
+                    if ((EW & colMask) == EW)
                         zoomCanvasFillRect((this.posX + i) * getBaseSize(), (this.posY + j) * getBaseSize(), getBaseSize() * 0.125, getBaseSize());
-                    if ((ER & colMask) == ER)
+                    if ((EE & colMask) == EE)
                         zoomCanvasFillRect((this.posX + i + 1) * getBaseSize(), (this.posY + j) * getBaseSize(), getBaseSize() * -0.125, getBaseSize());
                 }
             }
@@ -103,61 +95,39 @@ export class Player {
     tick() {
         this.frameDt = (Date.now() - this.prevTickTime) / 16;
         this.prevTickTime = Date.now();
-
-
-        let frameGravity = this.gravity;
-        if (this.kpyd) {
-            frameGravity *= 2;
-        }
-        if (this.kpyu) {
-            frameGravity /= 2;
-        }
-        this.speedY += frameGravity;
-
-        this.posX += this.speedX;
-        this.posY += this.speedY;
-
-        let startPx = this.posX;
-        let startPy = this.posY;
-
-        this.posX = Math.min(this.posX, getCanvasSquaresX() - this.sizeX);
-        this.posX = Math.max(0, this.posX);
-
-        this.posY = Math.max(0, this.posY);
-
-        if (startPx != this.posX) {
-            this.speedX = 0;
-        }
-        if (startPy != this.posY) {
-            this.speedY = 0;
-        }
-
         this.updateCollision();
         this.processCollision();
     }
 
     updateCollision() {
-        for (let proto of this.protos) {
+        for (let surface of [true, false]) {
             for (let i = 0; i < this.sizeX; i++) {
                 for (let j = 0; j <= this.sizeY; j++) {
-                    let pxc = Math.ceil(this.posX + i);
-                    let pyc = Math.ceil(this.posY + j);
-                    let pxf = Math.floor(this.posX + i);
-                    let pyf = Math.floor(this.posY + j);
+                    let pxn = Math.round(this.posX + i + 0.5);
+                    let pxs = pxn;
+                    let pxe = Math.round(this.posX + i);
+                    let pxw = Math.round(this.posX + i + 1);
 
-                    let ccc = getSquares(pxc, pyc).some((sq) => sq.proto == proto);
-                    let cff = getSquares(pxf, pyf).some((sq) => sq.proto == proto);
+                    let pyn = Math.round(this.posY + j);
+                    let pys = Math.round(this.posY + j + 1);
+                    let pye = Math.round(this.posY + j + 0.5);
+                    let pyw = pye;
 
-                    let ccf = getSquares(pxc, pyf).some((sq) => sq.proto == proto);
-                    let cfc = getSquares(pxf, pyc).some((sq) => sq.proto == proto);
+                    let xdt = this.frameDt * this.speedX;
+                    let ydt = this.frameDt * this.speedY;
+
+                    let cn = EN * getSquares(pxn + xdt, pyn + ydt).some((sq) => sq.surface == surface);
+                    let cs = ES * getSquares(pxs + xdt, pys + ydt).some((sq) => sq.surface == surface);
+                    let ce = EE * getSquares(pxe + xdt, pye + ydt).some((sq) => sq.surface == surface);
+                    let cw = EW * getSquares(pxw + xdt, pyw + ydt).some((sq) => sq.surface == surface);
 
                     let colMask = 0;
-                    colMask |= (cff || ccf) ? ET : 0; // top edge 
-                    colMask |= (ccf || ccc) ? EB : 0; // bottom edge
-                    colMask |= (cff || cfc) ? EL : 0; // left edge 
-                    colMask |= (ccf || ccc) ? ER : 0; // right edge;
+                    colMask |= cn
+                    colMask |= cs
+                    colMask |= ce
+                    colMask |= cw
 
-                    this.frameCollisionMap.get(proto).get(i).set(j, colMask);
+                    this.frameCollisionMap.get(surface).get(i).set(j, colMask);
                 }
             }
         }
@@ -165,58 +135,91 @@ export class Player {
     }
 
     processCollision() {
-        for (let proto of this.protos) {
-            let anyEB = false;
-            for (let i = 0; i < this.sizeX; i++) {
-                for (let j = 0; j < this.sizeY; j++) {
-                    anyEB |= (this.frameCollisionMap.get(proto).get(i).get(j) & EB) == EB;
+        // surface collision rules: 
+        // do not apply gravity 
+        // allow the player to move up or down at will, like left or right, at a set speed
+
+        // surface collision rules 
+        // the player 
+
+        let tickGravity = this.gravity;
+        let tickAcc = this.walkAcc;
+        let tickMax = this.walkMax;
+
+        let bottomSurfaceCollision = false;
+        let bottomNonSurfaceCollision = false;
+        let bottomMidNonSurfaceCollison = false;
+
+        for (let i = 0; i < this.sizeX; i++) {
+            for (let j = 0; j < this.sizeY; j++) {
+                bottomSurfaceCollision |= (this.frameCollisionMap.get(true).get(i).get(j) & ES) == ES;
+                bottomNonSurfaceCollision |= (this.frameCollisionMap.get(false).get(i).get(j) & ES) == ES;
+                if (j == this.sizeY - 2) {
+                    bottomMidNonSurfaceCollison |= (this.frameCollisionMap.get(false).get(i).get(j) & ES) == ES;
                 }
             }
-            this.bottomColMap.set(proto, anyEB);
         }
 
-        let acc, max, coll;
-        if (this.bottomColMap.get("SoilSquare")) {
-            acc = this.dirtAcc;
-            max = this.dirtMax;
-            coll = true;
-        } else if (this.bottomColMap.get("RockSquare")) {
-            acc = this.rockAcc;
-            max = this.rockMax;
-            coll = true;
+        this.jumpTicksLeft = Math.max(0, this.jumpTicksLeft - 1);
+
+        if (this.jumpTicksLeft > 0 && this.posY <= this.jumpStartY) {
+            // do nothing 
         } else {
-            acc = this.airAcc;
-            max = this.airMax;
+            if (bottomSurfaceCollision) {
+                tickGravity = 0;
+                this.speedY = 0;
+                if (this.kpyu)
+                    this.speedY = -tickAcc;
+                
+                if (!bottomNonSurfaceCollision && this.kpyd)
+                    this.speedY = tickAcc;
+
+            } else if (bottomNonSurfaceCollision) {
+                this.speedY = 0;
+                if (bottomMidNonSurfaceCollison) {
+                    tickGravity = -this.gravity * 6;
+                    if (this.speedX > 0) {
+                        this.speedX -= tickAcc * 2;
+                    } else {
+                        this.speedX += tickAcc * 2;
+                    }
+                } else {
+                    tickGravity = 0;
+                }
+            } else {
+                tickAcc = 0;
+            }
         }
-        
+        if (bottomNonSurfaceCollision || bottomSurfaceCollision) {
+            this.jumpTicks = 3;
+        }
+        this.speedY += tickGravity;
+        this.posX += this.speedX;
+        this.posY += this.speedY;
+
         if (this.kpxl)
-            this.speedX = Math.max(this.speedX - acc, -max);
+            this.speedX = Math.max(this.speedX - tickAcc, -tickMax);
         else if (this.kpxr)
-            this.speedX = Math.min(this.speedX + acc, max);
+            this.speedX = Math.min(this.speedX + tickAcc, tickMax);
         else {
             let sideX = (this.speedX > 0) ? 1 : -1;
             if (sideX > 0) {
-                this.speedX = Math.max(0, this.speedX - acc);
+                this.speedX = Math.max(0, this.speedX - tickAcc);
             } else {
-                this.speedX = Math.min(0, this.speedX + acc);
+                this.speedX = Math.min(0, this.speedX + tickAcc);
             }
         }
 
-        if (coll) {
-            this.posY -= .1;
-            this.speedY = 0;
-            this.collisionCacheMap[EB] = 4;
+        if (this.kpJump) {
+            this.jump();
         }
-
-        this.collisionCacheMap[ET] = Math.max(0, this.collisionCacheMap[ET] - 1);
-        this.collisionCacheMap[EB] = Math.max(0, this.collisionCacheMap[EB] - 1);
-        this.collisionCacheMap[EL] = Math.max(0, this.collisionCacheMap[EL] - 1);
-        this.collisionCacheMap[ER] = Math.max(0, this.collisionCacheMap[ER] - 1);
     }
 
     jump() {
-        if (this.collisionCacheMap[EB] > 0) {
-            this.collisionCacheMap[EB] = 0;
+        if (this.jumpTicks > 0 && this.jumpTicksLeft == 0) {
+            this.jumpTicks = 0;
+            this.jumpStartY = this.posY;
+            this.jumpTicksLeft = 10;
             this.speedY -= this.jumpSpeed;
         }
     }
@@ -226,13 +229,12 @@ export class Player {
             this.kpxl = true;
         if (key == this.kmxr)
             this.kpxr = true;
-
-        if (key == this.kmyu) {
-            this.jump();
+        if (key == this.kmyu) 
             this.kpyu = true;
-        }
         if (key == this.kmyd)
             this.kpyd = true;
+        if (key == this.kmJump)
+            this.kpJump = true;
     }
 
     handleKeyUp(key) {
@@ -244,5 +246,7 @@ export class Player {
             this.kpyu = false;
         if (key == this.kmyd)
             this.kpyd = false;
+        if (key == this.kmJump)
+            this.kpJump = false;
     }
 }
