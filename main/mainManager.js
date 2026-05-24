b4c33080e4841ee9864adf9dde04e8462e45d924
Devin Schmitz <devinschmitz2000@gmail.com>
2026-05-24 14:34:28 -0500

    checkpoint

--- main/mainManager.js
+++ main/mainManager.js
@@ -3,18 +3,50 @@ import { CanvasManager } from "./rendering/canvasManager.js"
 export class MainManager {
     constructor() {
         this.canvasManager = new CanvasManager("main");
+        this.canvasManager.addCallbacks(this);
+    }
+
+    main() {
+        this.frame();
+        setTimeout(() => this.main(), 0)
+    }
+
+    frame() {
+        this.update();
+        this.render();
+    }
+
+    update() {
+
+    }
+
+    render() {
+        this.canvasManager.render();
     }
 
     // Canvas Callbacks
-    mousemove() {}
-    mousedown() {}
-    mouseup() {}
-    touchstart() {}
-    touchend() {}
-    touchmove() {}
+    mousemove() { }
+    mousedown() { }
+    mouseup() { }
+
+    touchstart() { }
+    touchend() { }
+    touchmove() { }
+
+    onkeydown() { }
+    onkeyup() { }
+    onwheel() { }
+
+    // Canvas Event Listeners
+    mousemove(e) { }
+    mousedown(e) { }
+    mouseup(e) { }
+
+    drop(e) { }
 
     // Window Callbacks
-    onresize() {}
-    oncontextmenu() {}
-    onload() {}
+    onresize() { this.canvasManager.resize() }
+    oncontextmenu() { return false ;}
+
+
 }
--- main/rendering/canvasManager.js
+++ main/rendering/canvasManager.js
@@ -1,3 +1,6 @@
+import { HUEMAP } from "../color/hue.js";
+import { hsvToHex } from "../color/color.js";
+
 export class CanvasManager {
     constructor(elementId) {
         this.canvas = document.getElementById(elementId);
@@ -5,11 +8,34 @@ export class CanvasManager {
         this.width = 0;
         this.height = 0;
         
-        this.indexCanvasSize();
+        this.resize();
     }
 
-    indexCanvasSize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
+    resize() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+
+    render() {
+        this.context.fillStyle = hsvToHex((Date.now() / 10) % 360 + 60, .5, .8)
+        this.context.fillRect(
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
+        this.context.font =  this.canvas.width / 20 + "px courier";
+        this.context.fillStyle = "#000000"
+        this.context.fillText((new Date()).toISOString(), 0,this.canvas.height / 10);
+    }
+
+    addCallbacks(mainManager) {
+        this.canvas.addEventListener('mousemove', () => mainManager.mousemove, false);
+        this.canvas.addEventListener('mousedown', () => mainManager.mousedown);
+        this.canvas.addEventListener('mouseup', () => mainManager.mouseup);
+
+        this.canvas.onkeydown = () => mainManager.onkeydown;
+        this.canvas.onkeyup = () => mainManager.onkeyup;
+        this.canvas.onwheel = () => mainManager.onwheel;
     }
 }
