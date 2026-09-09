70f9dbf30018c6a10a342d31f4229719cc711e0b
Devin Schmitz <devinschmitz2000@gmail.com>
2026-09-09 18:46:00 -0500

    basic plant node flower start

--- static/organisms/flowers/leafNode/BaseLeafNodeFlower.js
+++ static/organisms/flowers/leafNode/BaseLeafNodeFlower.js
@@ -1,5 +1,5 @@
 import { randNumber, randRange } from "../../../common.js";
-import { STAGE_ADULT, STAGE_FLOWER, SUBTYPE_GRASS, SUBTYPE_NODE, SUBTYPE_STEM, TYPE_GRASS, TYPE_STEM } from "../../Stages.js";
+import { STAGE_ADULT, STAGE_FLOWER, SUBTYPE_GRASS, SUBTYPE_LEAF, SUBTYPE_NODE, SUBTYPE_STEM, TYPE_GRASS, TYPE_STEM } from "../../Stages.js";
 // import { GrowthPlan, GrowthPlanStep } from "../../../../GrowthPlan.js";
 import { GrowthPlan, GrowthPlanStep } from "../../GrowthPlan.js";
 import { BaseSeedOrganism } from "../../BaseSeedOrganism.js";
@@ -27,10 +27,8 @@ export class BaseLeafNodeFlower extends BaseOrganism {
 
 
         this.maxNumStem = 1;
-        this.maxStemLength = 12;
-
-        this.maxNumGrass = 2;
-        this.maxGrassLength = 5;
+        this.maxStemLength = 5;
+        this.maxLeafLength = 10;
 
         this.curNumGrass = 0;
         this.targetNumGrass = 1;
@@ -38,10 +36,12 @@ export class BaseLeafNodeFlower extends BaseOrganism {
 
         this.curNumStem = 0;
         this.targetNumStem = 0;
-        this.targetStemLength = 0;
+
+        this.targetStemLength = this.maxStemLength;
+        this.targetLeafLength = this.maxLeafLength;;
 
         this.stems = [];
-        this.grasses = [];
+        this.leaves = [];
     }
 
 
@@ -56,103 +56,59 @@ export class BaseLeafNodeFlower extends BaseOrganism {
     processGenetics() {
         super.processGenetics();
         let p0 = this.evolutionParameters[0];
-        this.maxNumGrass = randNumber(3, 5);
-        this.maxGrassLength = this.maxGrassLength + Math.floor(this.maxGrassLength * p0);
 
         this.maxNumStem = 1;
-        this.maxStemLength = 12;
+        this.maxStemLength = this.maxStemLength + Math.floor(this.maxStemLength * p0);
 
-        this.growthNumGreen = this.maxNumGrass * this.maxGrassLength;
+        this.growthNumGreen = this.maxStemLength * this.maxNumStem;
         this.growthNumRoots = this.growthNumGreen;
     }
 
     processLsqRendering() {
         if (!this.orgVisualUpdateFlag)
             return;
-        this.grasses.map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
-            .forEach((grass) => {
-                let l = grass.lifeSquares.length;
+        this.stems.map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
+            .forEach((stem) => {
+                let l = stem.lifeSquares.length;
                 let i = 0;
-                grass.lifeSquares.forEach((lsq) => {
+                stem.lifeSquares.forEach((lsq) => {
                     lsq.width = .3 + .3 * Math.log(3 + l - i);
                     i += 1;
                     this.applyColor(this.colorLeaf, i, lsq.renderColor);
                 });
-                grass.lifeSquares[0].theta = grass.lifeSquares[1]?.theta ?? grass.lifeSquares[0].theta;
+                stem.lifeSquares[0].theta = stem.lifeSquares[1]?.theta ?? stem.lifeSquares[0].theta;
+            })
+
+
+    this.leaves.map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
+            .forEach((stem) => {
+                let l = stem.lifeSquares.length;
+                let i = 0;
+                stem.lifeSquares.forEach((lsq) => {
+                    lsq.width = .3 + .3 * Math.log(3 + l - i);
+                    i += 1;
+                    this.applyColor(this.colorLeaf, i, lsq.renderColor);
+                });
+                stem.lifeSquares[0].theta = stem.lifeSquares[1]?.theta ?? stem.lifeSquares[0].theta;
             })
     }
 
-    prepareGrassGrowthPlanParams() {
-        this.grassTwist = randRange(.01, .3);
-        this.grassBaseRotation = 0;
-        this.grassBaseDeflection = randRange(.01, .3);;
-        this.grassBaseCurve = 0;
-        this.grassStrengthMult = .15;
-        this.grassRollingAveragePeriod = 150;
-    }
-
-    growGrass() {
-        if (this.curNumGrass > (this.curNumRoots / 4)) {
-            return;
-        }
-        this.prepareGrassGrowthPlanParams();
-        let startRootNode = this.getRootOrigin()
-        let growthPlan = new GrowthPlan(
-            startRootNode.posX, startRootNode.posY,
-            false, STAGE_ADULT,
-            randRange(-Math.PI, Math.PI),
-            this.grassTwist,
-            this.grassBaseRotation, this.grassBaseDeflection, this.grassBaseCurve,
-            TYPE_GRASS, this.grassStrengthMult, this.grassRollingAveragePeriod);
-
-        growthPlan.postConstruct = () => {
-            this.originGrowth.addChild(growthPlan.component);
-            this.grasses.push(this.originGrowth.getChildPath(growthPlan.component))
-        };
-        growthPlan.steps.push(new GrowthPlanStep(
-            growthPlan,
-            () => this.growGreenSquareAction(startRootNode, SUBTYPE_GRASS)
-        ))
-        this.growthPlans.push(growthPlan);
-        this.curNumGrass += 1;
-    }
-
-    lengthenGrass() {
-        this.grasses
-            .map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
-            .filter((grass) => grass.growthPlan.steps.length < this.targetGrassLength)
-            .forEach((grass) => {
-                let startNode = grass.lifeSquares.find((lsq) => lsq.subtype == SUBTYPE_GRASS);
-                if (startNode == null) {
-                    this.growthPlans = Array.from(this.growthPlans.filter((gp) => gp != grass.growthPlan));
-                    this.grasses = Array.from(this.grasses.filter((le) => this.originGrowth.getChildFromPath(le) != grass));
-                    this.curNumGrass -= 1;
-                    return;
-                }
-                for (let i = 0; i < this.targetGrassLength - grass.growthPlan.steps.length; i++) {
-                    grass.growthPlan.steps.push(new GrowthPlanStep(
-                        grass.growthPlan,
-                        () => this.growGreenSquareAction(startNode, SUBTYPE_GRASS)
-                    ))
-                };
-            });
-    }
-
     prepareStemGrowthPlanParams() {
-        this.stemTwist = randRange(.01, .1);
+        this.stemTwist = 0;
         this.stemBaseRotation = 0;
-        this.stemBaseDeflection = randRange(.01, .03);;
+        this.stemBaseDeflection = 0;
         this.stemBaseCurve = 0;
         this.stemStrengthMult = .35;
         this.stemRollingAveragePeriod = 150;
     }
 
     growStem() {
-        if (this.curNumStem > (this.curNumRoots / 4)) {
-            return;
-        }
         this.prepareStemGrowthPlanParams();
-        let startRootNode = this.getRootOrigin()
+        let startRootNode = this.originGrowth.lifeSquares.at(0);
+
+        if (this.curNumStem > 1)
+            startRootNode = this.getRootOrigin();
+
         let growthPlan = new GrowthPlan(
             startRootNode.posX, startRootNode.posY,
             false, STAGE_ADULT,
@@ -178,7 +134,7 @@ export class BaseLeafNodeFlower extends BaseOrganism {
             .map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
             .filter((stem) => stem.growthPlan.steps.length < this.targetStemLength)
             .forEach((stem) => {
-                let startNode = stem.lifeSquares.find((lsq) => lsq.subtype == SUBTYPE_GRASS);
+                let startNode = stem.lifeSquares.find((lsq) => lsq.subtype == SUBTYPE_STEM);
                 if (startNode == null) {
                     this.growthPlans = Array.from(this.growthPlans.filter((gp) => gp != stem.growthPlan));
                     this.stems = Array.from(this.stems.filter((le) => this.originGrowth.getChildFromPath(le) != stem));
@@ -188,22 +144,84 @@ export class BaseLeafNodeFlower extends BaseOrganism {
                 for (let i = 0; i < this.targetStemLength - stem.growthPlan.steps.length; i++) {
                     stem.growthPlan.steps.push(new GrowthPlanStep(
                         stem.growthPlan,
-                        () => this.growGreenSquareAction(startNode, SUBTYPE_GRASS)
+                        () => this.growGreenSquareAction(startNode, SUBTYPE_STEM)
                     ))
                 };
             });
     }
 
+    lengthenLeaves() {
+        this.leaves
+            .map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
+            .filter((leaf) => leaf.growthPlan.steps.length < this.targetLeafLength)
+            .forEach((leaf) => {
+                let startNode = leaf.lifeSquares.find((lsq) => lsq.subtype == SUBTYPE_LEAF);
+                if (startNode == null) {
+                    this.growthPlans = Array.from(this.growthPlans.filter((gp) => gp != leaf.growthPlan));
+                    this.leaves = Array.from(this.leaves.filter((le) => this.originGrowth.getChildFromPath(le) != leaf));
+                    this.curNumStem -= 1;
+                    return;
+                }
+                for (let i = 0; i < this.targetLeafLength - leaf.growthPlan.steps.length; i++) {
+                    leaf.growthPlan.steps.push(new GrowthPlanStep(
+                        leaf.growthPlan,
+                        () => this.growGreenSquareAction(startNode, SUBTYPE_LEAF)
+                    ))
+                };
+            });
+    }
+
+    leafStems() {
+        // adds 'leaves' to the stem at some interval
+        this.stems
+            .map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
+            .forEach((stem) => {
+                for (let i = 0; i < stem.lifeSquares.length; i++) {
+                    let c = stem.lifeSquares[i];
+                    if (c.leafNode == 1) {
+                        continue;
+                    }
+                    c.leafNode = 1;
+                    this.growLeafAtNode(c, (i & 1) ? 1 : -1); 
+                }
+            })
+        
+    }
+
+
+    prepareLeafGrowthParams(side) {
+        this.leafTwist = 0;
+        this.leafBaseRotation = 0;
+        this.leafBaseDeflection = 1;
+        this.leafBaseCurve = .125;
+        this.leafStrengthMult = .35;
+        this.leafRollingAveragePeriod = 150;
+    }
+
+    growLeafAtNode(startNode, side) {
+        this.prepareLeafGrowthParams(side);
+        let growthPlan = new GrowthPlan(
+            startNode.posX, startNode.posY,
+            false, STAGE_ADULT,
+            side * Math.PI * Math.random(),
+            this.leafTwist,
+            this.leafBaseRotation, this.leafBaseDeflection, this.leafBaseCurve,
+            TYPE_STEM, this.leafStrengthMult, this.leafRollingAveragePeriod);
+
+        growthPlan.postConstruct = () => {
+            this.originGrowth.addChild(growthPlan.component);
+            this.leaves.push(this.originGrowth.getChildPath(growthPlan.component))
+        };
+        growthPlan.steps.push(new GrowthPlanStep(
+            growthPlan,
+            () => this.growGreenSquareAction(startNode, SUBTYPE_LEAF)
+        ))
+
+        this.growthPlans.push(growthPlan);
+    }
 
     planGrowth() {
         if (!super.planGrowth()) {
-            return;
-        }
-
-        if (this.grasses
-            .map((parentPath) => this.originGrowth.getChildFromPath(parentPath))
-            .some((grass) => grass.growthPlan.steps.length < this.targetGrassLength)) {
-            this.lengthenGrass();
             return;
         }
 
@@ -214,33 +232,19 @@ export class BaseLeafNodeFlower extends BaseOrganism {
             return;
         }
 
-        if (this.targetGrassLength < (this.maxGrassLength / 2)) {
-            this.targetGrassLength += 1;
+        if (this.leafStems()) {
             return;
         }
 
-        if (this.targetNumStem < this.maxNumStem) {
-            this.targetNumStem += 1;
-            return;
-        }
-
-        if (this.targetNumGrass < this.maxNumGrass) {
-            this.targetNumGrass += 1;
-            return;
-        }
-
-        if (this.curNumGrass < this.targetNumGrass) {
-            this.growGrass();
-            return;
-        }
+        this.lengthenLeaves();
 
         if (this.curNumStem < this.targetNumStem) {
             this.growStem();
             return;
         }
 
-        if (this.targetGrassLength < this.maxGrassLength) {
-            this.targetGrassLength += 1;
+        if (this.targetNumStem < this.maxNumStem) {
+            this.targetNumStem += 1;
             return;
         }
 
